715ec71294476d7a421357dafa56c697e2b49300
gianelleeee <gianelleeee@gmail.com>
2023-04-17 19:30:06 +0800

    view_edit_delete

--- application/models/Products_model.php
+++ application/models/Products_model.php
@@ -20,4 +20,30 @@ class Products_model extends CI_Model{
         //saves the product into the database
         $this->db->insert('tblproducts', $data);
     }
+    
+    public function getProduct($id)
+    {
+        //loads the database
+        $this->load->database();
+        //makes the query using CI's query builder
+        $query = $this->db->get_where('tblproducts', array('prod_id'=>$id));
+        //runs the query then return the row
+        $product = $query->row();
+        //return the product's information from the database
+        return $product;
+    }
+
+    public function editProduct($id, $data)
+    {
+        $this->load->database();
+        $this->db->where('prod_id', $id);
+        $this->db->update('tblproducts', $data);
+    }
+
+    public function deleteProduct($id)
+    {
+        $this->load->database();
+        $this->db->where('prod_id', $id);
+        $this->db->delete('tblproducts');
+    }
 }
