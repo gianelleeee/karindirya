e395e0764b4c4f3e7a7dc4ca9ac4467573498977
gianelleeee <gianelleeee@gmail.com>
2023-04-17 19:29:33 +0800

    view_edit_delete

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -47,5 +47,73 @@ class Products extends CI_Controller{
         }
 
     }
-    
+
+    public function editProduct($id)
+    {
+        $this->load->helper(array('form', 'url'));
+        //load the product model
+        $this->load->model('Products_model');
+        //fetch the product using the id provided in the parameter
+        $data['product'] = $this->Products_model->getProduct($id);
+         $data['title'] = "Edit Product";
+        
+         //for checking if the view is edit or view item
+         $data['edit'] = true;
+         $this->load->view('include/header_view', $data);
+         $this->load->view('edit_product_view', $data);
+         $this->load->view('include/footer_view');
+    }
+
+    public function processEditProduct($id)
+    {
+         //load helpers
+         $this->load->helper(array('form', 'url'));
+         //includes the form validation library.
+        $this->load->library('form_validation');
+        $this->load->database();
+        $this->form_validation->set_rules('prod_name', 'Product Name', 'required');
+        $this->form_validation->set_rules('prod_description', 'Product Description', 'required');
+        $this->form_validation->set_rules('prod_price', 'Product Price', 'required|numeric');
+
+        //run form validation
+        if ($this->form_validation->run() == FALSE){
+            $this->editProduct($id);
+        }else {
+
+            $data = array(
+                'prod_name' => $this->input->post('prod_name'),
+                'prod_description' => $this->input->post('prod_description'),
+                'prod_price' => $this->input->post('prod_price')
+            );
+
+            //loads the product model
+            $this->load->model('Products_model');
+            $this->Products_model->editProduct($id, $data);
+            redirect('home/view_products');
+        }
+    }
+
+    public function viewProduct($id)
+    {
+        $this->load->helper(array('form', 'url'));
+        //load the product model
+        $this->load->model('Products_model');
+        //fetch the product using the id provided in the parameter
+        $data['product'] = $this->Products_model->getProduct($id);
+         $data['title'] = "Edit Product";
+        
+         //for checking if the view is edit or view item
+         $data['edit'] = false;
+         $this->load->view('include/header_view', $data);
+         $this->load->view('edit_product_view', $data);
+         $this->load->view('include/footer_view');
+    }
+
+    public function processDelete($id)
+    {
+        $this->load->helper('url');
+        $this->load->model('Products_model');
+        $this->Products_model->deleteProduct($id);
+        redirect('home/view_products');
+    }
 }
